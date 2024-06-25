e0280c769cce970ab3c6f8395d5db85382e9a9ae
Imamsubekti26 <imam0subekti@gmail.com>
2024-06-25 22:14:34 +0700

    fix

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -8,9 +8,18 @@
 </script>
 
 <div class="max-w-sm mx-auto p-6 border h-screen flex flex-col gap-4">
-  <TextInput placeholder="cari nama barang" />
+  <!-- <TextInput placeholder="cari nama barang" />
   <Button>aaa</Button>
   <CardInfo title="meja no.7" date="2024-09-24 07:40" price="60.000"/>
   <CategoryList name="milk" />
-  <CardMenu />
+  <CardMenu /> -->
+  <h1 class="font-semibold text-center">Aplikasi masih dalam proses development</h1>
+  <p>preview halaman :</p>
+  <ol>
+    <li><a class="underline text-blue-500" href="/auth/login"> - Login</a></li>
+    <li><a class="underline text-blue-500" href="/auth/login"> - Register</a></li>
+    <li><a class="underline text-blue-500" href="/d/transaksi"> - Transaksi</a></li>
+    <li><a class="underline text-blue-500" href="/d/tambah"> - Tambah Transaksi</a></li>
+    <li><a class="underline text-blue-500" href="/d/setting"> - setting</a></li>
+  </ol>
 </div>
